578030fac2a70a78ea92c0ed13c3868d10f41449
fatima916 <upulbandara269@gmail.com>
2025-07-09 14:37:28 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'upulbandara629@gmail.com',
+    password: 'chanukarm1#',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
